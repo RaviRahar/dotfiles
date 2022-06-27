f746703a114dfaf2b80cdc082474b613ade9325c
RaviRahar <ravirahar33@gmail.com>
2022-06-27 13:32:43 +0530

    Update: dapui config syntax change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
 if not vim.g.vscode then
   -- Setttings for vanilla nvim
-  require('core')
-  --pcall(require, 'core')
+  pcall(require, 'core')
   -- Plugins and their settings
-  require('plugins')
-  --pcall(require, 'plugins')
+  pcall(require, 'plugins')
 end
