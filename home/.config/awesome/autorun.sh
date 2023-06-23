6653e0d19237ee854cbb7a51638f3cf6080017b9
RaviRahar <ravirahar33@gmail.com>
2023-06-23 10:34:19 +0530

    Tried Fixing: power management

--- home/.config/awesome/autorun.sh
+++ home/.config/awesome/autorun.sh
@@ -8,9 +8,9 @@ run()
 }
 
 run setxkbmap -option ctrl:nocaps
-run xidlehook --detect-sleep --not-when-audio --timer 1 'dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1/session/self org.freedesktop.login1.Session.SetIdleHint boolean:true' 'dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1/session/auto org.freedesktop.login1.Session.SetIdleHint boolean:false'
-run setxkbmap -option ctrl:nocaps && xcape -e 'Caps_Lock=Escape' -t 100
+# run setxkbmap -option ctrl:nocaps && xcape -e 'Caps_Lock=Escape' -t 100
 run "${HOME}/.config/picom/picom.sh"
+run alacritty
+run xidlehook --detect-sleep --not-when-audio --timer 300 'systemctl suspend' ''
 run libinput-gestures-setup start
 run rog-control-center
-run alacritty
